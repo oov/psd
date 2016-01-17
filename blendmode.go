7af6303d44b09bdf86a011c178da8cdd78ed7f4a
oov <ilovevipper@gmail.com>
2016-01-17 22:02:45 +0900

    add BlendMode.String

--- blendmode.go
+++ blendmode.go
@@ -36,3 +36,68 @@ const (
 	BlendModeColor        = BlendMode("colr")
 	BlendModeLuminosity   = BlendMode("lum ")
 )
+
+// String implements fmt.Stringer interface.
+//
+// The return value respects blend name that is described in "Compositing and Blending Level 1"(https://www.w3.org/TR/compositing-1/#blending).
+func (bm BlendMode) String() string {
+	switch bm {
+	case BlendModePassThrough:
+		return "pass-through"
+	case BlendModeNormal:
+		return "normal"
+	case BlendModeDissolve:
+		return "dissolve"
+	case BlendModeDarken:
+		return "darken"
+	case BlendModeMultiply:
+		return "multiply"
+	case BlendModeColorBurn:
+		return "color-burn"
+	case BlendModeLinearBurn:
+		return "linear-burn"
+	case BlendModeDarkerColor:
+		return "darker-color"
+	case BlendModeLighten:
+		return "lighten"
+	case BlendModeScreen:
+		return "screen"
+	case BlendModeColorDodge:
+		return "color-dodge"
+	case BlendModeLinearDodge:
+		return "linear-dodge"
+	case BlendModeLighterColor:
+		return "lighter-color"
+	case BlendModeOverlay:
+		return "overlay"
+	case BlendModeSoftLight:
+		return "soft-light"
+	case BlendModeHardLight:
+		return "hard-light"
+	case BlendModeVividLight:
+		return "vivid-light"
+	case BlendModeLinearLight:
+		return "linear-light"
+	case BlendModePinLight:
+		return "pin-light"
+	case BlendModeHardMix:
+		return "hard-mix"
+	case BlendModeDifference:
+		return "difference"
+	case BlendModeExclusion:
+		return "exclusion"
+	case BlendModeSubtract:
+		return "subtract"
+	case BlendModeDivide:
+		return "divide"
+	case BlendModeHue:
+		return "hue"
+	case BlendModeSaturation:
+		return "saturation"
+	case BlendModeColor:
+		return "color"
+	case BlendModeLuminosity:
+		return "luminosity"
+	}
+	return "unknown-blend-name-" + string(bm)
+}
