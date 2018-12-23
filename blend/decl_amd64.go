ada9a92c125a73f698616e3b8b62c6b3bba4feeb
oov <ilovevipper@gmail.com>
2018-12-23 23:22:19 +0900

    blend: disable asm code
    This code is not working on some cpus.
    
    Ex: AMD Phenom(tm) II X6 1090T Processor 3.20Ghz

--- blend/decl_amd64.go
+++ blend/decl_amd64.go
@@ -3,5 +3,5 @@ package blend
 func drawNormalNRGBAToNRGBAFast(dest []byte, src []byte, alpha uint32, d0, s0, y int, sx0 int, sx1 int, sxDelta int, syDelta int, dx0 int, dx1 int, dxDelta int, dyDelta int)
 
 func init() {
-	drawNormalNRGBAToNRGBA = drawNormalNRGBAToNRGBAFast
+	// drawNormalNRGBAToNRGBA = drawNormalNRGBAToNRGBAFast
 }
